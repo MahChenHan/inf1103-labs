42ec058ab41e44e79373b1354890bc1eaada793f
MahChenHan <chenhanmah@gmail.com>
2026-09-04 17:29:15 +0800

    Instagram Profile

--- hello.py
+++ hello.py
@@ -2,7 +2,31 @@ print("===============================")
 print("Welcome here")
 print("My first post!")
 print("===============================")
-
+#Activity 1
 #3a) Yes
 #3b) Top to bottom
 #3c) In terminal
+
+username = "cool_creator"
+bio = "Fun blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Activity 2
+#2a) The use of the variables is to store information about the user, such as their username, bio, and number of followers. 
+#2b) Yes, the value will change.
+
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
